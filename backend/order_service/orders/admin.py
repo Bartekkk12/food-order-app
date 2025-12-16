df22338dcf46e3eb835c4468fcc85bd34afaab13
Bartosz Piróg <s168708@student.uken.krakow.pl>
2025-12-16 12:28:34 +0100

    Update db models

--- backend/order_service/orders/admin.py
+++ backend/order_service/orders/admin.py
@@ -4,7 +4,9 @@ from .models import *
 
 # Register your models here.
 
-admin.site.register(User)
 admin.site.register(Address)
+admin.site.register(User)
 admin.site.register(Restaurant)
+admin.site.register(Product)
 admin.site.register(Order)
+admin.site.register(OrderProducts)
--- backend/order_service/orders/models.py
+++ backend/order_service/orders/models.py
@@ -1,8 +1,24 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+
 from .managers import CustomUserManager
 
 # Create your models here.
+
+
+class Address(models.Model):
+    country = models.CharField(max_length=50, default="Poland")
+    city = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=10)
+    street = models.CharField(max_length=100)
+    house_number = models.CharField(max_length=50)
+    apartment_number = models.CharField(max_length=50, blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.street}, {self.house_number}, {self.apartment_number}"
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -11,6 +27,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     surname = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=20, unique=True)
+    address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True)
 
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
@@ -27,22 +44,32 @@ class User(AbstractBaseUser, PermissionsMixin):
         return self.email
 
 
-class Address(models.Model):
-    country = models.CharField(max_length=50, default="Poland")
-    city = models.CharField(max_length=100)
-    zip_code = models.CharField(max_length=10)
-    street = models.CharField(max_length=100)
-    house_number = models.CharField(max_length=50)
-    apartment_number = models.CharField(max_length=50, blank=True, null=True)
-
-    def __str__(self):
-        return f"{self.street}, {self.house_number}, {self.apartment_number}"
-
-
 class Restaurant(models.Model):
     restaurant_name = models.CharField(max_length=100)
-    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Order(models.Model):
-    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class OrderProducts(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='products')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
