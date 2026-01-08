06e937b30435b9df3827aef1628aa8dd15859585
Bartosz Piróg <s168708@student.uken.krakow.pl>
2026-01-08 14:18:03 +0100

    Add orders status update after payment

--- backend/payment_service/payments/services.py
+++ backend/payment_service/payments/services.py
@@ -1,7 +1,32 @@
 import time
+import pika
+import json
 
 
 def process_payment(order_id, total_price):
     print(f"[x] Processing payment for order {order_id}")
     time.sleep(3)
-    print(f"[✓] Payment for order {order_id} succeed.")
+    print(f"[✓] Payment for order {order_id} succeeded")
+
+    send_payment_success(order_id)
+
+
+def send_payment_success(order_id):
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host="rabbitmq", port=5672)
+    )
+    channel = connection.channel()
+
+    channel.queue_declare(queue="payment_success")
+
+    channel.basic_publish(
+        exchange="",
+        routing_key="payment_success",
+        body=json.dumps({
+            "order_id": order_id,
+            "status": "paid"
+        })
+    )
+
+    print(f"[+] Sent payment_success for order {order_id}")
+    connection.close()
